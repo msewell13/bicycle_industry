23a3a3b23c70be900d53ff4bffe465fc44490cb1
Matt Sewell <matt@pythonfinancial.com>
2017-12-27 20:00:24 +0000

    completed project

--- bicycles.py
+++ bicycles.py
@@ -3,22 +3,31 @@ class Bicycle(object):
         self.model = model
         self.weight = weight
         self.cost = cost
+        
+    def __repr__(self):
+        return "The bike model '{0}' weighs {1} lbs. and costs ${2} wholesale.".format(
+            self.model, self.weight, self.cost)
 
 
-class BikeShops(Bicycle):
-    def __init__(self, name, margin, bikes):
-        self.name = name
+class BikeShop(object):
+    def __init__(self, shop_name, margin, inventory):
+        self.shop_name = shop_name
         self.margin = margin
-        self.profit = 0
-        self.inventory = {}
-            
-    def sell_bike(self, margin, model, cost):
+        self.inventory = inventory
         
-        cost * margin / 100
-        # update inventory
         
-    def profit(self):
-        pass
+        # self.profit = 0
+        # init_inv = [('BMX', 3, 150), ('Huffy', 8, 250)]
+        # if inventory == None:
+        #     self.inventory = []
+        # for item in init_inv:
+        #     self.inventory.append(super(BikeShop, self).__init__(item[0], item[1], item[2]))
+        
+        # for bike in init_inv:
+        #     print(bike)
+        #     # bike.cost = bike[2]
+        #     # bike.markup = bike.cost * self.margin / 100
+        #     # bike.price = bike.cost + bike.markup
 
 
 # Have an inventory of different bicycles
@@ -26,19 +35,23 @@ class BikeShops(Bicycle):
 # Can see how much profit they have made from selling bikes
 
 
-class Customers(BikeShops):
+class Customers(BikeShop):
     def __init__(self, name, funds):
         self.name = name
         self.funds = funds
+        
+    def __repr__(self):
+        return "{0} has ${1} available to buy a bicycle".format(
+            self.name, self.funds)
     
-    def buy_bicycle(self, model, weight, cost):
-        if cost <= self.funds:
-            super(Customers, self).__init__(model, weight, cost)
-            self.funds = self.funds - cost
-        else:
-            print("Sorry {0}, you're too poor for this bicycle".format(self.name))
+    def bicycle(self, bike_name, retail_cost):
+        self.bike_name = bike_name
+        self.retail_cost = retail_cost
+        self.funds = self.funds - self.retail_cost
+            
+            
+            
+            # print("Sorry {0}, you're too poor for this bicycle"format(self.name))
         
         
         
-# Shop should have 6 different bicycles in stock
-# 3 customers with the following budgets 200, 500, 1000
--- main.py
+++ main.py
@@ -1,12 +1,61 @@
 from bicycles import *
+import random
 
 bikes = [Bicycle('BMX', 3, 150), Bicycle('Huffy', 8, 250), Bicycle('Schwinn', 10, 300),
 Bicycle('Classic Flyer', 7, 75), Bicycle('Diamondback', 12, 400), Bicycle('Mongoose', 9, 350)]
 
+
+# Create three customers. One customer has a budget of $200, the second $500, 
+# and the third $1000.
 customers = [Customers('Mark', 200), Customers('Chris', 500), Customers('Juan', 1000)]
 
-shop = BikeShops('The Bike Shop', 20, bikes)
+
+# Create a bicycle shop that has 6 different bicycle models in stock. The shop 
+# should charge its customers 20% over the cost of the bikes.
+shop = BikeShop('The Bike Shop', 20, bikes)
 
 
-if __name__ == '__main__':
+# Print the initial inventory of the bike shop for each bike it carries.
+print("\n welcome to {}. Here is a list of our current inventory:\n".format(shop.shop_name))
+for item in shop.inventory:
+    print(item)
+
+
+# Print the name of each customer, and a list of the bikes offered by the bike 
+# shop that they can afford given their budget. Make sure you price the bikes in 
+# such a way that each customer can afford at least one.
+for customer in customers:
+    print('\n')
+    print(customer, 'and can afford the following bikes:')
+    for bike in shop.inventory:
+        retail_price = int(bike.cost + (bike.cost * shop.margin / 100))
+        if retail_price <= customer.funds:
+            print('{0}: ${1}'.format(bike.model, retail_price))
+
+
+#Have each of the three customers purchase a bike then print the name 
+# of the bike the customer purchased, the cost, and how much money they 
+# have left over in their bicycle fund.
+for customer in customers:
+    choose_bike = random.choice(shop.inventory)
+    retail_price = int(choose_bike.cost + (choose_bike.cost * shop.margin / 100))
+    while retail_price > customer.funds:
+        choose_bike = random.choice(shop.inventory)
+        retail_price = int(choose_bike.cost + (choose_bike.cost * shop.margin / 100))
+    bike_purchase = customer.bicycle(choose_bike.model, retail_price)
+    print("\n{0} purchased a {1} for ${2}".format(customer.name, 
+    choose_bike.model, retail_price))
+    print("{0} has ${1} remaining in their bike fund\n".format(customer.name, customer.funds))
+    shop.inventory.remove(choose_bike)
     
+    # After each customer has purchased their bike, the script should print out 
+    # the bicycle shop's remaining inventory for each bike, and how much profit 
+    # they have made selling the three bikes.
+    print("\n Remaining inventory:")
+    for bike in shop.inventory:
+        print(bike)
+    print("\n {0} has made a profit of {1}".format(shop.shop_name, retail_price - choose_bike.cost))
+    
+        
+        
+
